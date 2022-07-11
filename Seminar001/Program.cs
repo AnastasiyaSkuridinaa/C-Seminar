4f206cc8cd1f12a03c8f16b7faa437b6447f2d21
Skuridina Anastasiya <skuridinan@yandex.ru>
2022-07-11 12:31:04 +0300

    commit

---
+++ Seminar001/Program.cs
@@ -0,0 +1,22 @@
+﻿/*// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!"); // writeLine - метод 
+
+
+
+int a=2,b=3;
+int s=a+b;
+Console.WriteLine(a+"+"+b+"="+s);//склеивание строк
+System.Console.WriteLine("{2}+{1}={0}",s,b,a);//строка форматирования
+System.Console.WriteLine($"{a}+{b}={s}");//строка интерполяции
+
+
+//Показать четные числа от 1 до N
+int i=0;
+int N=20;
+while(i<=N)
+{
+    System.Console.Write($"{i} ");
+    i=i+2;
+}
+*/
+
